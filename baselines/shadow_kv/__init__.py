43e37c8b036315532358ff8321c1ec98f8b20d9c
Yoongon-Kim <yooongonkim@gmail.com>
2026-06-13 18:19:11 +0900

    baseline clear

--- baselines/shadow_kv/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import SHADOWKV_CONFIG
-from .adapter import ShadowKVLLMAdapter, build_shadowkv_llm
-
-__all__ = ["SHADOWKV_CONFIG", "ShadowKVLLMAdapter", "build_shadowkv_llm"]
--- baselines/snap_kv/upstream/__init__.py
+++
@@ -1,4 +0,0 @@
-from .snapkv.monkeypatch.snapkv_utils import init_snapkv
-from .snapkv.monkeypatch.monkeypatch import replace_llama
-
-__all__ = ["init_snapkv", "replace_llama"]
